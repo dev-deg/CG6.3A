d72d5029d4ae241f465a2669d42ee0d5281991e1
deg <david.deguara@mcast.edu.mt>
2022-10-14 08:30:56 +0200

    Added singleton pattern

---
+++ Assets/Scripts/FirebaseStorageController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseStorageController : MonoBehaviour
+{
+    //Singleton
+    public static FirebaseStorageController Instance
+    {
+        get;
+        private set;
+    }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        Instance = this;
+    }
+
+}
