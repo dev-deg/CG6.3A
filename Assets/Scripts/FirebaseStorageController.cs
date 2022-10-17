e5e4df2ed26b2a3f11e329e62eb76cceac96f817
deg <david.deguara@mcast.edu.mt>
2022-10-17 13:45:46 +0200

    Added Manifest file and working on thumbnail container

--- Assets/Scripts/FirebaseStorageController.cs
+++ Assets/Scripts/FirebaseStorageController.cs
@@ -12,8 +12,13 @@ public class FirebaseStorageController : MonoBehaviour
 {
     
     private FirebaseStorage _firebaseStorageInstance;
-    private Image _imagePlaceholder;
-    private RawImage _rawImagePlaceholder;
+    [SerializeField] private GameObject RawImagePrefab;
+    private GameObject _thumbnailContainer;
+
+    public enum DownloadType
+    {
+        Thumbnail, Manifest
+    }
     
     //Singleton
     public static FirebaseStorageController Instance
@@ -34,37 +39,68 @@ public class FirebaseStorageController : MonoBehaviour
 
     private void Start()
     {
-        _imagePlaceholder = GameObject.Find("Downloaded_Image").GetComponent<Image>();
-        _rawImagePlaceholder = GameObject.Find("Downloaded_Raw_Image").GetComponent<RawImage>();
-        DownloadImageAsync("gs://cg-01-7bb16.appspot.com/Thumbnails/Image1.png");
+        _thumbnailContainer = GameObject.Find("Thumbnails_Container");
+        DownloadFileAsync("gs://cg-01-7bb16.appspot.com/manifest.txt",DownloadType.Manifest);
     }
 
-    public void DownloadImageAsync(String url)
+    public void DownloadFileAsync(String url, DownloadType dType)
     {
         // Create a storage reference from our storage service
         StorageReference imageRef =
             _firebaseStorageInstance.GetReferenceFromUrl(url);
-        
+
         // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
         const long maxAllowedSize = 1 * 1024 * 1024;
-        imageRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task => {
-            if (task.IsFaulted || task.IsCanceled) {
+        imageRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
                 Debug.LogException(task.Exception);
                 // Uh-oh, an error occurred!
             }
-            else {
+            else
+            {
                 byte[] fileContents = task.Result;
                 Debug.Log($"{imageRef.Name} finished downloading!");
-                //Display the image inside _imagePlaceholder
-                Texture2D tex = new Texture2D(1, 1);
-                tex.LoadImage(fileContents);
-                _rawImagePlaceholder.texture = tex;
-                
-                Sprite spr = Sprite.Create(tex,new Rect(0,0,tex.width,tex.height),
-                    new Vector2(tex.width/2,tex.height/2));
-                _imagePlaceholder.sprite = spr;
-                
+                if (dType == DownloadType.Thumbnail)
+                {
+                    //Load Image
+                    StartCoroutine(LoadImage(fileContents));
+                }
+                else if (dType == DownloadType.Manifest)
+                {
+                    //Load the manifest
+                    StartCoroutine(LoadManifest(fileContents));
+                }
+
             }
         });
+    }
+
+    IEnumerator LoadManifest(byte[] fileContents)
+        {
+            //Converting from byte array to String UTF8
+            string s = System.Text.Encoding.UTF8.GetString(fileContents, 0, fileContents.Length);
+            //Parsing the manifest text file by splitting each url separately
+            string[] urls = s.Split("\n");
+            yield return null;
+            foreach (string url in urls)
+            {
+                print(url);
+            }
+        }    
+
+    IEnumerator LoadImage(byte[] fileContents)
+    {
+        // //Display the image inside _imagePlaceholder
+        // Texture2D tex = new Texture2D(1, 1);
+        // tex.LoadImage(fileContents);
+        // _rawImagePlaceholder.texture = tex;
+        //         
+        // Sprite spr = Sprite.Create(tex,new Rect(0,0,tex.width,tex.height),
+        //     new Vector2(tex.width/2,tex.height/2));
+        // _imagePlaceholder.sprite = spr;
+        yield return null;
+        Debug.Log("Image Loaded Successfully!");
     }
 }
